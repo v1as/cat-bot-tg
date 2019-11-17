302f9ea5758fb52fbd46a18530ecfc11ca4d381e
ivlasishen <KOLlok08>
2019-11-17 21:52:55 +0300

    HungryCatPhase added

---
+++ src/main/java/ru/v1as/tg/cat/callbacks/phase/curios_cat/HungryCatPhase.java
@@ -0,0 +1,58 @@
+package ru.v1as.tg.cat.callbacks.phase.curios_cat;
+
+import static ru.v1as.tg.cat.callbacks.is_cat.CatRequestVote.CAT1;
+import static ru.v1as.tg.cat.callbacks.is_cat.CatRequestVote.NOT_CAT;
+import static ru.v1as.tg.cat.utils.RandomUtils.random;
+
+import java.util.function.Consumer;
+import org.springframework.stereotype.Component;
+import ru.v1as.tg.cat.EmojiConst;
+import ru.v1as.tg.cat.callbacks.phase.poll.ChooseContext;
+
+@Component
+public class HungryCatPhase extends AbstractCuriosCatPhase {
+
+    @Override
+    protected void open() {
+        messages(
+                "В этот раз кот никуда не идёт.",
+                "Да и выглядит он каким-то...",
+                "Голодным.",
+                "Случайности ли, но совсем рядом продуктовый магазин.");
+        poll("Покормим котика?")
+                .choice("Покормить", this::buyMeal)
+                .choice("Кот! " + EmojiConst.CAT, this::catchTheCat)
+                .send();
+    }
+
+    private void catchTheCat(ChooseContext chooseContext) {
+        messages(
+                "Кот смотрит на вас взглядом, полным вселенской скорби.",
+                "Но вы не поддаётесь, и получаете свой заслуженный балл.");
+        catchUpCatAndClose(CAT1);
+    }
+
+    private void buyMeal(ChooseContext chooseContext) {
+        messages(
+                "Выходя из магазина, вы довольно размахиваете пакетиком с кормом.",
+                "Вы горды собой и предвкушаете как сделаете доброе дело.");
+        Consumer<ChooseContext> next = random(this::feedTheCat, this::feedTheCatFail);
+        next.accept(chooseContext);
+    }
+
+    private void feedTheCat(ChooseContext chooseContext) {
+        messages(
+                "Кот сразу же замечает угощенье в ваших руках.",
+                "Он вьётся вокруг ваших ног, а его мяуканье слышно на соседней улице.",
+                "Наблюдая за тем как содержимое пакетика тает на глазах, вы получаете свой заслуженный балл.");
+        catchUpCatAndClose(CAT1);
+    }
+
+    private void feedTheCatFail(ChooseContext chooseContext) {
+        messages(
+                "Но, оказавшись на улице, вы понимаете, что остались одни.",
+                "Видимо, кот был не так уж и голоден.");
+        catchUpCatAndClose(NOT_CAT);
+    }
+
+}
--- src/main/java/ru/v1as/tg/cat/utils/TimeoutUtils.java
+++ src/main/java/ru/v1as/tg/cat/utils/TimeoutUtils.java
@@ -7,7 +7,10 @@ import lombok.extern.slf4j.Slf4j;
 @UtilityClass
 public class TimeoutUtils {
 
+    private static final int MIN_READING_TIMEOUT_MS = 1500;
+    private static final int SYMBOL_TIMEOUT_MS = 70;
+
     public static int getMsForTextReading(int symbols) {
-        return Math.max(1500, symbols * 70);
+        return Math.max(MIN_READING_TIMEOUT_MS, symbols * SYMBOL_TIMEOUT_MS);
     }
 }
