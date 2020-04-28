5019e82b6b303888474f823faf4a45b0d0ce64ae
ivlasishen <KOLlok08>
2020-04-28 09:34:37 +0300

    Changed priority for players

--- src/main/java/ru/v1as/tg/cat/callbacks/phase/impl/JoinCatFollowPhase.java
+++ src/main/java/ru/v1as/tg/cat/callbacks/phase/impl/JoinCatFollowPhase.java
@@ -2,6 +2,7 @@ package ru.v1as.tg.cat.callbacks.phase.impl;
 
 import static java.time.LocalDateTime.now;
 import static java.time.temporal.ChronoUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static ru.v1as.tg.cat.EmojiConst.CAT;
 
 import com.google.common.collect.ImmutableList;
@@ -114,7 +115,10 @@ public class JoinCatFollowPhase extends AbstractPhase<Context> {
         if (recentlyCatsAmount == 0) {
             goToCat(data);
         } else {
-            botClock.schedule(contextWrap(() -> goToCat(data)), 2, TimeUnit.SECONDS);
+            botClock.schedule(
+                    contextWrap(() -> goToCat(data)),
+                    Math.min(2_000 + recentlyCatsAmount * 500, 10_000),
+                    MILLISECONDS);
         }
     }
 
