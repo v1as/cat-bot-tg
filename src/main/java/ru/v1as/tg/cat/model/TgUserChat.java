12d4a10eff1f2295e6ad6e2c79443318ba8e4d8a
v1as <uvele@mail.ru>
2020-05-12 22:59:05 +0300

    test command improved

---
+++ src/main/java/ru/v1as/tg/cat/model/TgUserChat.java
@@ -0,0 +1,30 @@
+package ru.v1as.tg.cat.model;
+
+public class TgUserChat implements TgChat {
+
+    private final TgUser user;
+
+    public TgUserChat(TgUser user) {
+        this.user = user;
+    }
+
+    @Override
+    public Long getId() {
+        return user.getChatId();
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public Boolean isUserChat() {
+        return true;
+    }
+}
